5b53dd11ec57cfb76a70f902c5e9e6e4991f58f3
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-02-09 10:52:13 -0600

    Add answers to challenge 3

--- for.js
+++ for.js
@@ -23,6 +23,12 @@ for (var i = 0; i < weather_options.length; i++) {
 // Use a for statement to log to the console only the words that are exactly 3 characters in length
 var words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
 
+for (var i = 0; i < words.length; i++) {
+  if (words[i].length === 3) {
+    console.log(words[i]);
+  }
+}
+
 // 📹 Record yourself live-coding and talking through any ONE of the challenges. 
   // The recording doesn't have to be the first time you are solving the problem. 
   // Use technical vocabulary and explain each part to demonstrate your understanding! 
